ac09d3917bcccef63935835be5012dd7d02b2247
daniloantoniomacedo <daniloantoniomacedo@gmail.com>
2021-05-20 12:49:04 -0300

    Adição do jogo da forca.

--- src/Jogos/Main.java
+++ src/Jogos/Main.java
@@ -1,15 +1,16 @@
 package Jogos;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static Scanner Leia = new Scanner(System.in);
+	public static String nomeP1;
 
 	public static void main(String[] args) {
 		
 		int opcao, condicao;
-		String nomeP1;
 		
 		imprimir("Informe o seu nome: ");
 		nomeP1 = Leia.nextLine();
@@ -19,6 +20,7 @@ public class Main {
 			opcao = menu();
 			switch (opcao){
 			case 1:
+				executarJogoForca();
 				break;
 			case 2:
 				break;
@@ -39,7 +41,7 @@ public class Main {
 	
 	public static int menu() {
 		int opcao;
-		System.out.println("##---------- Jogos ----------##\n");
+		System.out.println("\n##---------- Jogos ----------##\n");
 		System.out.println("|-----------------------------|");
 		System.out.println("| Opção 1 - Jogo da Forca     |");
 		System.out.println("| Opção 2 - Batalha Naval     |");
@@ -60,6 +62,245 @@ public class Main {
 		System.out.print(txt);
 	}
 	
+	//--------------------------------------------------------------Jogo da Forca-----------------------------------------------------------------//
+	
+	public static int countVitorias;
+	public static int countDerrotas;
+	public static int countLetras;
+	public static char resposta;
+	
+	public static void executarJogoForca() {
+		
+		System.out.println("\nJogo da Forca selecionado!");
+
+		char[] letras = gerarPalavra();	
+		String[] tracejado = new String[countLetras];
+
+		while (countDerrotas <= 7){
+			gerarForca();
+			tracejado = gerarTracejado(letras, tracejado);
+			if (verificarVitoria(letras, tracejado) == true) {
+				imprimirResultado(true);
+				break;
+			}
+			obterResposta();
+			validarCaracter(letras);	
+			if (countDerrotas >= 7) {
+				gerarForca();
+				imprimirResultado(false);
+				break;
+			}
+		}
+		
+	}
+
+	public static char[] gerarPalavra() {
+
+		int index;
+		String palavra;
+
+		String[] dados = new String[5];
+		dados[0] = "Abacaxi";
+		dados[1] = "Goiaba";
+		dados[2] = "Berinjela";
+		dados[3] = "Tomate";
+		dados[4] = "Uva";
+		Random aleatorio = new Random();
+		index = aleatorio.nextInt(4+1);
+		palavra = dados[index];	
+
+		char[] letras = new char[palavra.length()];
+
+		for (int i=0; i<palavra.length(); i++) {
+			letras[i] = palavra.charAt(i);
+		}
+
+		countLetras = palavra.length();
+
+		return letras;
+
+	}
+
+	public static void validarCaracter(char[] letras) {
+
+		boolean acerto = false;
+
+		for (int i=0; i<letras.length; i++) {
+			if (Character.toLowerCase(letras[i]) == Character.toLowerCase(resposta)) {
+				acerto = true;
+				break;
+			}
+		}
+		
+		if (acerto == true) {
+			countVitorias++;
+		}else {
+			countDerrotas++;
+		}
+	}
+	
+	public static boolean verificarVitoria(char[] letras, String[] tracejado) {
+		String resposta = " ";
+		String gabarito = " ";
+		boolean resultado = false;
+		
+		for (int i = 0; i<tracejado.length; i++) {
+			resposta += tracejado[i].replace(" ", "");
+		}
+		
+		for (int i = 0; i<letras.length; i++) {
+			gabarito += letras[i];
+		}
+		
+		if (resposta.toLowerCase().equals(gabarito.toLowerCase())) {
+			resultado = true;
+		}
+		
+		return resultado;
+		
+	}
+	
+	public static void gerarForca() {
+
+		if (7-countDerrotas>0) {
+			System.out.printf("\nVocê deverá acertar a palavra \n"
+					+ "em %d tentativa(s) para vencer o jogo. \n", 7-countDerrotas);
+		}
+
+		switch (countDerrotas) {
+		case 0: 
+
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 1: 
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 2: 
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |          |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 3: 
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |         /|");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 4: 
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |         /|\\");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 5:
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |         /|\\");
+			System.out.println(" |          |");
+			System.out.println(" |");
+			System.out.println("_|______________");
+
+			break;
+		case 6: 
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |         /|\\");
+			System.out.println(" |          |");
+			System.out.println(" |         / ");
+			System.out.println("_|______________");
+
+			break;
+		case 7:
+			System.out.println("  __________");
+			System.out.println(" |          |");
+			System.out.println(" |          0");
+			System.out.println(" |         /|\\");
+			System.out.println(" |          |");
+			System.out.println(" |         / \\");
+			System.out.println("_|______________");
+
+			break;
+		}
+
+	}
+
+	public static String[] gerarTracejado(char[] letras, String[] tracejado) {
+
+		if (countVitorias < 1) {
+			for (int i=0; i < letras.length; i++) {
+				tracejado[i] = "_ ";
+			}
+
+		}else{
+
+			for (int i=0; i<letras.length; i++) {
+				if (i == 0 && Character.toLowerCase(letras[i]) == Character.toLowerCase(resposta)) {
+					tracejado[i] = Character.toUpperCase(resposta) + " ";
+				}
+				if(i != 0 && Character.toLowerCase(letras[i]) == Character.toLowerCase(resposta)) {
+					tracejado[i] = Character.toLowerCase(resposta) + " ";
+				}
+
+			}
+		}
+		for (int i=0; i<letras.length; i++) {
+			System.out.print(tracejado[i]);
+		}
+		
+		return tracejado;
+
+	}
+
+	public static void obterResposta() {
+
+		System.out.println();
+		System.out.println("Digite uma letra:");
+		resposta = Leia.next().charAt(0);
+
+	}
+	
+	public static void imprimirResultado(boolean retorno) {
+		if (retorno == true) {
+			
+			System.out.println();
+			System.out.printf("\nParabéns, %s! Você ganhou. \n", nomeP1);
+			
+		}else {
+			
+			System.out.printf("\n%s, Você perdeu. \n", nomeP1);
+			
+		}
+	}
+
 	//--------------------------------------------------------------Jogo da Velha-----------------------------------------------------------------//
 	
 	public static void jogoDaVelha() {
