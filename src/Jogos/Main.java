4b0575b10f34a0a1599b10666307c94185dc6c86
daniloantoniomacedo <daniloantoniomacedo@gmail.com>
2021-05-20 16:41:17 -0300

    Jogo da forca adicionado.

--- src/Jogos/Main.java
+++ src/Jogos/Main.java
@@ -99,12 +99,7 @@ public class Main {
 		int index;
 		String palavra;
 
-		String[] dados = new String[5];
-		dados[0] = "Abacaxi";
-		dados[1] = "Goiaba";
-		dados[2] = "Berinjela";
-		dados[3] = "Tomate";
-		dados[4] = "Uva";
+		String[] dados = {"Abacaxi", "Goiaba", "Berinjela", "Tomate", "Uva"};
 		Random aleatorio = new Random();
 		index = aleatorio.nextInt(4+1);
 		palavra = dados[index];	
@@ -296,7 +291,7 @@ public class Main {
 			
 		}else {
 			
-			System.out.printf("\n%s, Você perdeu. \n", nomeP1);
+			System.out.printf("\n%s, você perdeu. \n", nomeP1);
 			
 		}
 	}
